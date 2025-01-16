0cd7f83476ae4d9d8bd565e68ce18632924df93e
Rai-sys <railaneserafim484@gmail.com>
2025-01-16 17:28:08 -0300

    add: atividade não terminada (16/01/2025)

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const { Curso } = require("./src/models/Curso");
 const alunos = new AlunoController()
 
 //alunos.adicionarAluno('Fulano', 'fulando1@gmail.com', '4002-8922', '1234', 'Moda')
-alunos.adicionarAluno('Bianca Vitoria', 'bianca03@gmail.com', '8776-5421', '111214', 'Engenharia')
+//alunos.adicionarAluno('Bianca Vitoria', 'bianca03@gmail.com', '8776-5421', '111214', 'Engenharia')
+alunos.editarAluno('111214', 'Bianca Vivienne', null, null, null)  // null para preservar o dado que nao quero que seja editado
+
+alunos.excluirAluno('20250101');
+
 alunos.listarAluno();
